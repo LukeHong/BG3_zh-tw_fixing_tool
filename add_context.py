c9a97af94d68e9e5e2ab4726455ff759193efead
Luke Hong <luke0724@hotmail.com>
2024-03-10 16:24:52 +0800

    Update add context file path

--- add_context.py
+++ add_context.py
@@ -1,28 +1,38 @@
 import csv
-import xml.etree.ElementTree as ET
 import html
+import os
+import sys
+import xml.etree.ElementTree as ET
+
+BASE_PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 contexts = {}
-tree = ET.parse('english.xml')
+tree = ET.parse(os.path.join(BASE_PATH, 'source', 'english.xml'))
+
 root = tree.getroot()
 
 for child in root:
-    contexts[child.attrib['contentuid']] = html.escape(child.text, quote=True)
-del(tree)
-del(root)
+    contexts[child.attrib['contentuid']] = child.text #html.escape(child.text, quote=True)
+del tree, root
 
+print(f'English contexts: {len(contexts)} rows')
 
 data = []
-with open('TS.csv', 'r', encoding='UTF-8') as read_file:
+update_count = 0
+with open(os.path.join(BASE_PATH, 'translated', 'TS.csv'), 'r', encoding='UTF-8') as read_file:
     reader = csv.reader(read_file, delimiter=',')
     for row in reader:
         row.append('')
+        row[1] = html.unescape(row[1])
         if row[0] in contexts:
+            update_count += 1
             row[-1] = contexts[row[0]]
 
         data.append(row)
 
-with open('TS_contexted.csv', 'w', newline='', encoding='UTF-8') as write_file:
+print(f'{update_count} rows of context added.')
+
+with open('TS.csv', 'w', newline='', encoding='UTF-8') as write_file:
     writer = csv.writer(write_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
     for row in data:
         writer.writerow(row)
